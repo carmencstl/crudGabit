6a2f6e576c24d80a65513677a097600779793218
Carmen <ccastillogaitan12@gmail.com>
2025-12-28 00:17:32 +0100

    asjd

--- config/Request.php
+++ config/Request.php
@@ -2,44 +2,44 @@
 
 namespace CrudGabit\Config;
 
-    class Request
+class Request
+{
+    /**
+     * Obtener valor de POST o GET
+     * @param string $key
+     * @return string|null
+     */
+    public static function get(string $key): ?string
     {
-
-        /**
-         * Obtener valor de POST o GET
-         * @param string $key
-         * @return string|null
-         */
-        public static function get(string $key): ?string
-        {
-            return $_POST[$key] ?? $_GET[$key] ?? null;
-        }
-
-        /**
-         * Obtener todos los datos POST
-         * @return array
-         */
-        public static function post(?string $key = null) : array|string|null
-        {
-            if ($key) {
-                return $_POST[$key] ?? null;
-            }
-
-            return $_POST;
-        }
-
-        /**
-         * Redirigir a URL con base /crudGabit
-         * @param string $url
-         * @return never
-         */
-        public static function redirect(string $path): void
-        {
-            if (!str_contains($path, "/crudGabit")) {
-                $path = "/crudGabit" . (str_starts_with($path, "/") ? $path : "/" . $path);
-            }
-            header("Location: {$path}");
-            exit;
-        }
-
+        return $_POST[$key] ?? $_GET[$key] ?? null;
     }
+
+    /**
+     * Obtener todos los datos POST
+     * @return array
+     */
+    public static function post(?string $key = null) : array|string|null
+    {
+        if ($key) {
+            return $_POST[$key] ?? null;
+        }
+
+        return $_POST;
+    }
+
+    /**
+     * Redirigir a URL (SIN añadir /crudGabit)
+     * @param string $path
+     * @return never
+     */
+    public static function redirect(string $path): void
+    {
+        // Asegurar que empiece con /
+        if (!str_starts_with($path, "/")) {
+            $path = "/" . $path;
+        }
+        
+        header("Location: {$path}");
+        exit;
+    }
+}
